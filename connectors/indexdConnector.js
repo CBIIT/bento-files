d91fc631c53d0ac0e5f2ac7a50585ff283ea06f4
Eric Miller <eric.miller4@nih.gov>
2025-03-11 17:44:41 -0400

    debugging

--- connectors/indexdConnector.js
+++ connectors/indexdConnector.js
@@ -6,9 +6,8 @@ const getJSON = bent(config.indexDUrl, "json");
 module.exports = async function (file_id) {
   try {
     let result = await getJSON(`/${file_id}`);
-    console.log("Result:  ", result);
     return result.url;
   } catch (error) {
-    console.log("Error fetching file data from indexD!", error);
+    console.error(error);
   }
 };
