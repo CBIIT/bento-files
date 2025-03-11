5bfa0e45611a6fbeed64b9ea1ff81a0d69371c96
Eric Miller <eric.miller4@nih.gov>
2025-03-11 17:20:31 -0400

    add try/catch to indexD request

--- connectors/indexdConnector.js
+++ connectors/indexdConnector.js
@@ -1,9 +1,13 @@
-const bent = require('bent');
-const config = require('../config');
+const bent = require("bent");
+const config = require("../config");
 
-const getJSON = bent(config.indexDUrl, 'json')
+const getJSON = bent(config.indexDUrl, "json");
 
 module.exports = async function (file_id) {
-  let result = await getJSON(`/${file_id}`);
-  return result.url;
-}
+  try {
+    let result = await getJSON(`/${file_id}`);
+    return result.url;
+  } catch (error) {
+    console.log(error);
+  }
+};
