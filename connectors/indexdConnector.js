25836c146a7d20f5fa9656ea0e46a134af3a8e24
Eric Miller <eric.miller4@nih.gov>
2025-03-12 15:45:57 -0400

    add error handling to indexd url fetching

--- connectors/indexdConnector.js
+++ connectors/indexdConnector.js
@@ -1,9 +1,16 @@
-const bent = require('bent');
-const config = require('../config');
+const bent = require("bent");
+const config = require("../config");
 
-const getJSON = bent(config.indexDUrl, 'json')
+const getJSON = bent(config.indexDUrl, "json");
 
 module.exports = async function (file_id) {
-  let result = await getJSON(`/${file_id}`);
-  return result.url;
-}
+  try {
+    let result = await getJSON(`/${file_id}`);
+    return result.url;
+  } catch (error) {
+    console.error(`Error fetching file from IndexD: ${file_id}`, error);
+    let err = new Error("Retrieving pre-signed URL failed");
+    err.status = 500;
+    throw err;
+  }
+};
